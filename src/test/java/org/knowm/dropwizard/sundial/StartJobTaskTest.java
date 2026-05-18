1e9331118575d7d6c79275d39fa2d5fc4a966f8d
Tim Molter <tim.molter@gmail.com>
2026-05-18 19:04:26 +0200

    refactor: update test dependencies to JUnit 5 and Mockito, remove PowerMock

--- src/test/java/org/knowm/dropwizard/sundial/StartJobTaskTest.java
+++ src/test/java/org/knowm/dropwizard/sundial/StartJobTaskTest.java
@@ -1,6 +1,8 @@
 package org.knowm.dropwizard.sundial;
 
-import com.google.common.collect.ImmutableMap;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.mockStatic;
+
 import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
@@ -9,75 +11,61 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import org.easymock.EasyMock;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
-import org.junit.runner.RunWith;
+
+import org.junit.jupiter.api.Test;
 import org.knowm.dropwizard.sundial.tasks.StartJobTask;
 import org.knowm.sundial.SundialJobScheduler;
-import org.powermock.api.easymock.PowerMock;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
+import org.mockito.MockedStatic;
 
-@RunWith(PowerMockRunner.class)
-@PrepareForTest(SundialJobScheduler.class)
-public class StartJobTaskTest {
-  @Rule public ExpectedException thrown = ExpectedException.none();
+class StartJobTaskTest {
 
   @Test
-  public void shouldStartNamedTask() throws Exception {
+  void shouldStartNamedTask() throws Exception {
     StartJobTask task = new StartJobTask();
-    Map<String, List<String>> map = new HashMap<String, List<String>>();
-
+    Map<String, List<String>> map = new HashMap<>();
     map.put("JOB_NAME", Arrays.asList("test"));
     OutputStream stream = new ByteArrayOutputStream();
     PrintWriter out = new PrintWriter(stream);
 
-    PowerMock.mockStatic(SundialJobScheduler.class);
-    SundialJobScheduler.startJob("test", Collections.singletonMap("JOB_NAME", (Object) "test"));
-    EasyMock.expectLastCall();
-
-    PowerMock.replay(SundialJobScheduler.class);
-
-    task.execute(map, out);
-
-    PowerMock.verify(SundialJobScheduler.class);
+    try (MockedStatic<SundialJobScheduler> mockedStatic = mockStatic(SundialJobScheduler.class)) {
+      task.execute(map, out);
+      mockedStatic.verify(
+          () ->
+              SundialJobScheduler.startJob(
+                  "test", Collections.singletonMap("JOB_NAME", (Object) "test")));
+    }
   }
 
   @Test
-  public void shouldPassParameters() throws Exception {
+  void shouldPassParameters() throws Exception {
     StartJobTask task = new StartJobTask();
-    Map<String, List<String>> map = new HashMap<String, List<String>>();
+    Map<String, List<String>> map = new HashMap<>();
     map.put("JOB_NAME", Arrays.asList("test"));
     map.put("Param1", Arrays.asList("1"));
     map.put("Param2", Arrays.asList("2"));
     OutputStream stream = new ByteArrayOutputStream();
     PrintWriter out = new PrintWriter(stream);
 
-    PowerMock.mockStatic(SundialJobScheduler.class);
-    SundialJobScheduler.startJob(
-        "test",
-        ImmutableMap.of(
-            "JOB_NAME", (Object) "test", "Param1", (Object) "1", "Param2", (Object) "2"));
-    EasyMock.expectLastCall();
+    Map<String, Object> expectedParams = new HashMap<>();
+    expectedParams.put("JOB_NAME", "test");
+    expectedParams.put("Param1", "1");
+    expectedParams.put("Param2", "2");
 
-    PowerMock.replay(SundialJobScheduler.class);
-
-    task.execute(map, out);
-
-    PowerMock.verify(SundialJobScheduler.class);
+    try (MockedStatic<SundialJobScheduler> mockedStatic = mockStatic(SundialJobScheduler.class)) {
+      task.execute(map, out);
+      mockedStatic.verify(() -> SundialJobScheduler.startJob("test", expectedParams));
+    }
   }
 
   @Test
-  public void shouldGiveErrorMessageIfNoJobNameSpecified() throws Exception {
+  void shouldGiveErrorMessageIfNoJobNameSpecified() throws Exception {
     StartJobTask task = new StartJobTask();
-    Map<String, List<String>> map = new HashMap<String, List<String>>();
+    Map<String, List<String>> map = new HashMap<>();
     OutputStream stream = new ByteArrayOutputStream();
     PrintWriter out = new PrintWriter(stream);
 
-    thrown.expect(IllegalArgumentException.class);
-    thrown.expectMessage("JOB_NAME");
-    task.execute(map, out);
+    assertThatThrownBy(() -> task.execute(map, out))
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessageContaining("JOB_NAME");
   }
 }
--- src/test/java/org/knowm/dropwizard/sundial/SundialBundleWithoutStopTaskIT.java
+++ src/test/java/org/knowm/dropwizard/sundial/SundialBundleWithoutStopTaskIT.java
@@ -2,13 +2,14 @@ package org.knowm.dropwizard.sundial;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
 import io.dropwizard.client.JerseyClientBuilder;
 import io.dropwizard.core.setup.Environment;
 import io.dropwizard.testing.junit5.DropwizardAppExtension;
 import jakarta.ws.rs.client.Client;
-import org.junit.Test;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 
 public class SundialBundleWithoutStopTaskIT extends SundialBundleITBase {
   public static DropwizardAppExtension<TestConfiguration> app =
